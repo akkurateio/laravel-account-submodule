5867dd6d49c1432102c597e1ea943df05976cbde
Camille <camille@subvitamine.com>
2021-01-19 11:49:50 +0100

    Add helpers function to get the User and Account classes wherever you are

--- helpers.php
+++ helpers.php
@@ -26,3 +26,67 @@ if (!function_exists('currentAccount')) {
 
     }
 }
+
+if (!function_exists('account')) {
+    /**
+     * @return \Akkurate\LaravelAccountSubmodule\Models\Account|\App\Models\Account
+     */
+    function account()
+    {
+
+        if(class_exists(\App\Models\Account::class)) {
+
+            return new \App\Models\Account();
+        }
+
+        return new \Akkurate\LaravelAccountSubmodule\Models\Account();
+    }
+}
+
+if (!function_exists('accountClass')) {
+    /**
+     * @return string
+     */
+    function accountClass(): string
+    {
+
+        if(class_exists(\App\Models\Account::class)) {
+
+            return \App\Models\Account::class;
+        }
+
+        return \Akkurate\LaravelAccountSubmodule\Models\Account::class;
+    }
+}
+
+if (!function_exists('user')) {
+    /**
+     * @return \Akkurate\LaravelAccountSubmodule\Models\User|\App\Models\User
+     */
+    function user()
+    {
+
+        if(class_exists(\App\Models\User::class)) {
+
+            return new \App\Models\User();
+        }
+
+        return new \Akkurate\LaravelAccountSubmodule\Models\User();
+    }
+}
+
+if (!function_exists('userClass')) {
+    /**
+     * @return string
+     */
+    function userClass(): string
+    {
+
+        if(class_exists(\App\Models\User::class)) {
+
+            return \App\Models\User::class;
+        }
+
+        return \Akkurate\LaravelAccountSubmodule\Models\User::class;
+    }
+}
