9732cb3c1c4cf0f0eb8686f9acbc5d8504cd9302
Sébastien Pereda <sebastien@subvitamine.com>
2021-01-19 08:42:34 +0100

    Update helpers.php

--- helpers.php
+++ helpers.php
@@ -4,20 +4,25 @@ if (!function_exists('currentAccount')) {
     function currentAccount()
     {
 
-        if(! class_exists(\App\Models\Account::class)) {
-            return null;
-        }
+        if(class_exists(\App\Models\Account::class)) {
 
-        $account = \App\Models\Account::where('slug', request('uuid'))
-            ->with(['preference'])
-            ->first();
-
-        if(empty($account)) {
-            $account = \App\Models\Account::where(auth()->user()->account->slug)
+            $account = \App\Models\Account::where('slug', request('uuid'))
                 ->with(['preference'])
                 ->first();
+
+        } else {
+
+            $account = \Akkurate\LaravelAccountSubmodule\Models\Account::where('slug', request('uuid'))
+                ->with(['preference'])
+                ->first();
+
+        }
+
+        if(empty($account)) {
+            $account = auth()->user()->account;
         }
 
         return $account;
+
     }
 }
